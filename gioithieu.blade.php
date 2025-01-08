dce58466d6cc80ca9da30324d92e05116cdb751c
QuysPhus <quyphu27102003@gmail.com>
2025-01-08 22:26:16 +0700

    Add files via upload

---
+++ gioithieu.blade.php
@@ -0,0 +1,50 @@
+<div class="card mb-3 shadow-5" style="background-color: #EEEEEE">
+    <div class="card-body" style="margin-top:40px" >
+        <center>
+            <h3 class="card-title">GIỚI THIỆU</h3>
+        </center>
+    </div>
+    <br>
+</div>
+
+<div class="container">
+    <br>
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="/trang-chu">Trang chủ</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Giới thiệu</li>
+        </ol>
+    </nav>
+    <br>
+
+    <div class="card mb-3" style="max-width: 100%;">
+        <div class="row g-0">
+            <div class="col-xl-3">
+                <img src="{{ URL('images1/gioithieu.png') }}" alt="..." class="img-fluid rounded" />
+            </div>
+
+            <div class="col-xl-9">
+                <div class="card-body">
+                    <h5 class="card-title">DIRTY SHOP</h5>
+                    <br>
+                    <div class="card-text">
+                        <p>Cửa hàng của chúng tôi là nơi bạn tìm thấy những bộ quần áo phong cách, phù hợp với mọi xu hướng và sở thích. Chúng tôi cung cấp các sản phẩm từ áo phông, quần jean đến những bộ váy thanh lịch, luôn đảm bảo chất lượng và sự thoải mái. Với các thiết kế thời thượng, bạn sẽ luôn tự tin và nổi bật mọi lúc, mọi nơi. </p>
+                        <p>Chúng tôi chuyên cung cấp những bộ quần áo đa dạng, từ năng động, trẻ trung đến thanh lịch, tinh tế. Với cam kết mang đến những sản phẩm chất lượng và cập nhật xu hướng mới nhất, cửa hàng của chúng tôi là lựa chọn hoàn hảo để bạn thể hiện phong cách cá nhân. Hãy đến và tìm ngay những bộ trang phục yêu thích cho mùa này!</p>
+
+                    </div>
+                    <p class="card-text">
+                        <i><small class="text-success">Đăng ký tài khoản để bắt đầu mua sắm nào bạn ơi!</small></i>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <br>
+    <br>
+    <br>
+
+
+</div>
